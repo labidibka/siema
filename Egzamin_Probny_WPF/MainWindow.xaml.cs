46fcd293a3bc05c1a94921570aa911b77be0ed0c
labidibka <labidibka@gmail.com>
2025-03-11 11:17:49 +0100

    wswsws

--- Egzamin_Probny_WPF/MainWindow.xaml.cs
+++ Egzamin_Probny_WPF/MainWindow.xaml.cs
@@ -23,29 +23,31 @@ namespace Egzamin_Probny_WPF
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            int liczba_znakow = int.Parse(znaki.Text);
+
             Random random = new Random();
-            string[] tablica = new string[10];
+            string[] tablica = new string[liczba_znakow];
             string[] litery = { "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM" };
             string[] liczby = { "0123456789" };
             string[] specjalne = {"!@#$%^&*()_+-="};
 
             for (int i = 0; i < tablica.Length; i++) 
             {
-                if (Wielkie_male.IsChecked == true)
+                if (Wielkie_male.IsChecked == true && Cyfry.IsChecked == false && Specjalne.IsChecked == false )
                 {
-                    tablica[0] = litery[random.Next(0, litery.Length + 1)];
+                    tablica[0] = litery[random.Next(0, litery.Length)];
                 }
-                else if (Cyfry.IsChecked == true)
+                else if (Cyfry.IsChecked == true && Wielkie_male.IsChecked == false && Specjalne.IsChecked == false) 
                 {
-                    tablica[1] = liczby[random.Next(0, liczby.Length + 1)];
+                    tablica[1] = liczby[random.Next(0, liczby.Length)];
                 }
-                else if (Specjalne.IsChecked == true) 
+                else if (Specjalne.IsChecked == true && Cyfry.IsChecked == false && Wielkie_male.IsChecked == false) 
                 {
-                    tablica[2] = specjalne[random.Next(0, specjalne.Length + 1)];
+                    tablica[2] = specjalne[random.Next(0, specjalne.Length)];
                 }
             }
 
-            MessageBox.Show($"Losowe Hasło Wynosi: {tablica[0]} ");
+            MessageBox.Show($"Losowe Hasło Wynosi: {tablica[2]} ");
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
